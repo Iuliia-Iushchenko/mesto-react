2fd58fd18fd589d68472253c5f1f06af20439f18
Iuliia Iushchenko <julia.uschenko@yandex.ru>
2020-07-23 02:04:05 +1000

    Updates

---
+++ precache-manifest.374bed8ef971655d9d812c6be40f5961.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26358fd75f6d5f535aff950358114b79",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7f5179b176aaa8c19d2e",
+    "url": "./static/css/main.6e6fec6e.chunk.css"
+  },
+  {
+    "revision": "2a509e54112e51c55b81",
+    "url": "./static/js/2.78a60301.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.78a60301.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5179b176aaa8c19d2e",
+    "url": "./static/js/main.2d360eb3.chunk.js"
+  },
+  {
+    "revision": "341c23a9630937c9de88",
+    "url": "./static/js/runtime-main.06cca9be.js"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "./static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "./static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "./static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "c709803c3cab6f1116039e881ecf531a",
+    "url": "./static/media/Inter-Medium.c709803c.woff2"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "./static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "./static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "./static/media/add_button.b2fce4ac.svg"
+  },
+  {
+    "revision": "5df32a06fdf69f3ae19600017c36d45a",
+    "url": "./static/media/close_button.5df32a06.svg"
+  },
+  {
+    "revision": "d80783439d382b46ce24b8da369767d4",
+    "url": "./static/media/delite.d8078343.svg"
+  },
+  {
+    "revision": "b05083e51f8e21ef50c2b78b29bb740c",
+    "url": "./static/media/edit_button.b05083e5.svg"
+  },
+  {
+    "revision": "83a46f45b0ce22c9a4433c11d0372f26",
+    "url": "./static/media/like.83a46f45.svg"
+  },
+  {
+    "revision": "5bbb05f725aa334a73acf3174e255f4e",
+    "url": "./static/media/like_active.5bbb05f7.svg"
+  },
+  {
+    "revision": "c6f110191388042ab958ac5e9b285425",
+    "url": "./static/media/logo.c6f11019.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "./static/media/penсil.d476a8b8.svg"
+  },
+  {
+    "revision": "183d8bcab94f1c0d5fd5a8dcb93fc172",
+    "url": "./static/media/profile_image.183d8bca.jpg"
+  }
+]);
